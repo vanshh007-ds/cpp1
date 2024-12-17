83c5860c2494214efb497172ffff0ac819e97448
vanshh007-ds <vanshdagar12071982@gmail.com>
2024-12-17 14:01:58 +0530

    lengthofstring.cpp

---
+++ lengthofstring.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+char name[10]="vansh";
+int i,length=0;
+for(i=0;name[i]!= '\0';i++)
+{
+length++;
+    
+}
+    cout<<"The length of string is : "<<length;
+    return 0;
+}
